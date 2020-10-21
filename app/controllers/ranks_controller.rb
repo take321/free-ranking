7dfaea7b8c283c605ac9f2e515281419fb243292
take321 <70375053+take321@users.noreply.github.com>
2020-10-21 12:05:11 +0900

    pry-rails 導入create記述

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -7,4 +7,20 @@ class RanksController < ApplicationController
   def new
     @ranking = Rank.new
   end
+
+  def create
+    @ranking = Rank.new(ranking_params)
+    if @ranking.valid?
+      render 'rank_numbers/new'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def ranking_params
+    params.require(:rank).permit(:title, :text)
+  end
 end
