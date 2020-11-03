cc5d59ff1e9bc4b3140d15fa652f99bc37700e4c
take321 <70375053+take321@users.noreply.github.com>
2020-11-03 16:30:52 +0900

    削除機能の実装

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -37,10 +37,27 @@ class RanksController < ApplicationController
   #   end
   # end
 
+  def destroy
+    ranking = Rank.find(params[:id])
+    ranks = RankNumber.where(rank_id: params[:id])
+    if delete_ranks(ranks) && ranking.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
 
   private
 
   def ranking_params
     params.require(:rank_create).permit(:title, :text, :category_id, :rank_number_ids, rank_numbers: [:rank_number_id1, :rank_title1, :rank_text1, :rank_number_id2, :rank_title2, :rank_text2, :rank_number_id3, :rank_title3, :rank_text3, :rank_number_id4, :rank_title4, :rank_text4, :rank_number_id5, :rank_title5, :rank_text5, :rank_number_id6, :rank_title6, :rank_text6, :rank_number_id7, :rank_title7, :rank_text7, :rank_number_id8, :rank_title8, :rank_text8, :rank_number_id9, :rank_title9, :rank_text9, :rank_number_id10, :rank_title10, :rank_text10]).merge(user_id: current_user.id)
   end
+
+  def delete_ranks(ranks)
+    ranks.each do |rank|
+      rank.destroy
+    end
+  end
+  
 end
